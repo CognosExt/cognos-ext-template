e475e426aa03236a70375599f5cf72ff788cdd82
Reinier Battenberg <reinier@mountbatten.nl>
2019-04-09 11:35:26 +0200

    feat: Manage Settings from Settings.json

--- grunt/cognos_ext_upload.js
+++ grunt/cognos_ext_upload.js
@@ -1,12 +1,30 @@
+try {
+  var settings = require('../Settings.json');
+} catch (e) {
+  console.error(
+    chalk.red(
+      'Settings.json not valid. Copy SettingsTemplate.json to Settings.json and fill in the correct values.'
+    )
+  );
+  process.exit(1);
+}
+
+var pack = require('../package.json');
+
+var pack = require('../package.json');
+var regex = /\./gi;
+var filename = pack.version.replace(regex, '_');
+var file = 'dist/extension-' + filename + '.zip';
+
 module.exports = {
   default: {
     options: {
-      name: 'Module_Name',
-      url: 'https://example.com/ibmcognos',
+      name: pack.name,
+      url: settings.url,
       type: 'extensions', // or themes
-      user: 'tester',
-      password: 'secret',
-      debug: false,
+      user: settings.user,
+      password: settings.password,
+      debug: settings.debug,
       zipfile: 'dist/extension.zip'
     }
   }
