577e14469a9dd6d4693f623680960b8d47b38b7b
Reinier Battenberg <reinier@mountbatten.nl>
2019-04-09 11:36:14 +0200

    feat: Add version number to filename

--- grunt/zip.js
+++ grunt/zip.js
@@ -1,11 +1,12 @@
-// Inside your Gruntfile.js
+var pack = require('../package.json');
+var regex = /\./gi;
+var filename = pack.version.replace(regex, '_');
+var file = 'dist/extension-' + filename + '.zip';
+
 module.exports = {
   default: {
-    src:
-       [
-        'dist/**'
-      ],
-      cwd: 'src',
-    dest: 'dist/extension.zip'
-    }
+    src: ['dist/**'],
+    cwd: 'dist',
+    dest: file
   }
+};
